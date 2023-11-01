fd03d818b899956faa5b61b118fcd73988aefbac
cutti034 <cutti034@umn.edu>
2023-11-01 17:46:17 -0500

    Partially functional calendar

--- lib/Views/Calendar.dart
+++ lib/Views/Calendar.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatelessWidget {
+  DateTime focusedDay = DateTime.now();
+  DateTime firstDay = DateTime.now().subtract(Duration(days: 365));
+  DateTime lastDay = DateTime.now().add(Duration(days: 1826));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,10 +13,18 @@ class Calendar extends StatelessWidget {
         title: Text('Calendar'),
       ),
       body: Center(
-        child: Text('This is the Calendar page'),
+        child: TableCalendar(
+          locale: "en_US",
+          rowHeight: 50,
+          headerStyle: HeaderStyle(formatButtonVisible: false, titleCentered: true),
+          availableGestures: AvailableGestures.all,
+          focusedDay: focusedDay,
+          firstDay: firstDay,
+          lastDay: lastDay,
+          //onDaySelected: onDaySelected,
+        )
       ),
       backgroundColor: Colors.lightGreen[100],
-
     );
   }
 }
