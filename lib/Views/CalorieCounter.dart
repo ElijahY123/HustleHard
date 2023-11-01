461dfeef393d06a0dba94b1a7eeea836ce8700af
amach013 <amach013@d.umn.edu>
2023-11-01 17:39:47 -0500

    Added buttons and a counter for calories

--- lib/Views/CalorieCounter.dart
+++ lib/Views/CalorieCounter.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class CalorieCounter extends StatelessWidget {
+class CalorieCounter extends StatefulWidget {
+  @override
+  _CalorieCounterState createState() => _CalorieCounterState();
+}
+
+class _CalorieCounterState extends State<CalorieCounter> {
+  int totalCalories = 0;
+
+  void addCalories(int amount) {
+    setState(() {
+      totalCalories += amount;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +21,30 @@ class CalorieCounter extends StatelessWidget {
         title: Text('Calorie Counter'),
       ),
       body: Center(
-        child: Text('This is the Calorie Counter page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Total Calories: $totalCalories', style: TextStyle(fontSize: 24)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    addCalories(1); // Add 1 calorie when the plus button is pressed
+                  },
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(width: 16), // Add some spacing between the buttons
+                ElevatedButton(
+                  onPressed: () {
+                    addCalories(-1); // Subtract 1 calorie when the minus button is pressed
+                  },
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       backgroundColor: Colors.lightGreen[100],
     );
