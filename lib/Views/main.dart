67f9f273f06656bec10b9b8e08b9f64d4e27a183
Ethan Ness <ness0321@d.umn.edu>
2023-11-05 01:32:59 -0600

    Finally got MVC Working.
    Implemented the MVC framework

--- lib/Views/main.dart
+++ lib/Views/main.dart
@@ -1,13 +1,12 @@
 import 'package:firstapp/Controller/controller.dart';
-import 'package:firstapp/Views/MainPage.dart';
 import 'package:flutter/material.dart';
-//import 'CalorieCounter.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,97 +15,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
       ),
-      home: FitnessView(),
-    );
-
-  }
-}
-
-/**
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = MainPage();
-        break;
-      case 1:
-        page = PlaceHolderPage();
-        break;
-      case 2:
-        page = CalorieCounter();
-        break;
-      default:
-        throw UnimplementedError('No page for $selectedIndex');
-    }
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home Page'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.hourglass_empty),
-                      label: Text('Placeholder page'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.fastfood),
-                      label: Text('Calorie Counter'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      },
+      home: FitnessController(),
     );
   }
 }
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Workout Application Framework Main Page', textAlign: TextAlign.center),
-    );
-  }
-}
-
-class PlaceHolderPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Placeholder second page', textAlign: TextAlign.center),
-    );
-  }
-}
-*/
