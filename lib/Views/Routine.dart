d320d25cffc48a24d8a8cb5175bae75e53997c98
Ethan Ness <ness0321@d.umn.edu>
2023-11-14 16:02:53 -0600

    Made page Scrollable
    Made the page with the routine table scrollable so if you make the table large you can scroll to see it all

--- lib/Views/Routine.dart
+++ lib/Views/Routine.dart
@@ -1,155 +1,159 @@
-//import 'dart:html';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RoutineView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
    return Scaffold(
      appBar: AppBar(
        title: Text('Workout Routines'),
      ),
 
      body: Center(
-    child: Table(
-       border: TableBorder.all(),
-       columnWidths: const <int, TableColumnWidth>{
-         0: FlexColumnWidth(),
-         1: FlexColumnWidth(),
-         2: FlexColumnWidth(),
-       },
-       defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-       children: <TableRow>[
-         TableRow(
-           children: <Widget>[
-             Container(
-               child: Center(
-                child: Text('Workout schedule for Arms', textScaleFactor: 1.5,),
-               ),
-             ),
-             TableCell(
-               verticalAlignment: TableCellVerticalAlignment.top,
-               child: Container(
-                 child: Center(
-                  child: Text('Workout schedule for Legs'),
+
+    child: ListView(
+      children: [
+         Table(
+           border: TableBorder.all(),
+           columnWidths: const <int, TableColumnWidth>{
+             0: FlexColumnWidth(),
+             1: FlexColumnWidth(),
+             2: FlexColumnWidth(),
+           },
+           defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+           children: <TableRow>[
+             TableRow(
+               children: <Widget>[
+                 Container(
+                   child: Center(
+                    child: Text('Workout schedule for Arms', textScaleFactor: 1.5,),
+                   ),
                  ),
-               ),
+                 TableCell(
+                   verticalAlignment: TableCellVerticalAlignment.top,
+                   child: Container(
+                     child: Center(
+                      child: Text('Workout schedule for Legs', textScaleFactor: 1.5,),
+                     ),
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                     child: Text('Workout schedule for Core', textScaleFactor: 1.5,)
+                   ),
+                 ),
+               ],
              ),
-             Container(
-               child: Center(
-                 child: Text('Workout schedule for Core')
+             TableRow(
+               decoration:  const BoxDecoration(
                ),
+               children: <Widget>[
+                 Container(
+                   child: Center(
+                     child: Text('Monday\n' 'Dumbbell Curls\n' 'Scarecrow Extensions\n' 'SpeedBag\n'),
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                     child: Text('Monday\n' 'Squats\n' 'Lateral Lung\n' 'Leg Press\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Monday\n' 'Side Bridges\n' 'Raised Leg Hold\n' 'Plank Arm Raises\n')
+                   ),
+                 ),
+               ],
              ),
-           ],
-         ),
-         TableRow(
-           decoration:  const BoxDecoration(
-           ),
-           children: <Widget>[
-             Container(
-               child: Center(
-                 child: Text('Monday\n' 'Dumbell Curls\n' 'Scarecrow Extensions\n' 'SpeedBag\n'),
+             TableRow(
+               decoration: const BoxDecoration(
                ),
+               children: <Widget>[
+                 Container(
+                   child: Center(
+                     child: Text('Tuesday\n' 'Cross Over Extensions\n' 'Hammer Curls\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                     child: Text('Tuesday\n' 'Knee Highs\n' 'Single Leg Dead-lift\n' 'Split Squat\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                     child: Text('Tuesday\n' 'Push Up Into Lunges\n' 'Knee to Elbows\n' 'Side to Side Chops\n')
+                   ),
+                 ),
+               ],
              ),
-             Container(
-               child: Center(
-                 child: Text('Monday\n' 'Squats\n' 'Lateral Lung\n' 'Leg Press\n')
+             TableRow(
+               decoration: const BoxDecoration(
                ),
+               children: <Widget>[
+                 Container(
+                   child: Center(
+                       child: Text('Wednesday\n' 'Overhead Triceps Extension\n' 'Side Curls\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Wednesday\n' 'Skater Squat\n' 'Tuck Jump\n' 'Reverse Lung\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Wednesday\n' 'Cross Chops\n' 'High Knees\n' 'Twist Jacks\n')
+                   ),
+                 ),
+               ],
              ),
-             Container(
-               child: Center(
-                   child: Text('Monday\n' 'Side Bridges\n' 'Raised Leg Hold\n' 'Plank Arm Raises\n')
+             TableRow(
+               decoration: const BoxDecoration(
                ),
+               children: <Widget>[
+                 Container(
+                   child: Center(
+                       child: Text('Thursday\n' 'Around the Worlds\n' 'Seated Curls\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Thursday\n' 'Adductor Leg Raises\n' 'Hamstring Curls\n' 'Romanian Dead-lifts\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Thursday\n' 'Side Leg Raises\n' 'Crunch Kicks\n' 'Raised Leg Circles\n')
+                   ),
+                 ),
+               ],
              ),
-           ],
-         ),
-         TableRow(
-           decoration: const BoxDecoration(
-           ),
-           children: <Widget>[
-             Container(
-               child: Center(
-                 child: Text('Tuesday\n' 'Cross Over Extensions\n' 'Hammer Curls\n')
+             TableRow(
+               decoration: const BoxDecoration(
                ),
-             ),
-             Container(
-               child: Center(
-                 child: Text('Tuesday\n' 'Knee Highs\n' 'Single Leg Deadlift\n' 'Split Squat\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                 child: Text('Tuesday\n' 'Push Up Into Lunges\n' 'Knee to Elbows\n' 'Side to Side Chops\n')
-               ),
-             ),
-           ],
-         ),
-         TableRow(
-           decoration: const BoxDecoration(
-           ),
-           children: <Widget>[
-             Container(
-               child: Center(
-                   child: Text('Wednesday\n' 'Overhead Tricep Extension\n' 'Side Curls\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                   child: Text('Wednesday\n' 'Skater Squat\n' 'Tuck Jump\n' 'Reverse Lung\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                   child: Text('Wednesday\n' 'Cross Chops\n' 'High Knees\n' 'Twist Jacks\n')
-               ),
-             ),
-           ],
-         ),
-         TableRow(
-           decoration: const BoxDecoration(
-           ),
-           children: <Widget>[
-             Container(
-               child: Center(
-                   child: Text('Thursday\n' 'Around the Worlds\n' 'Seated Curls\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                   child: Text('Thursday\n' 'Adductor Leg Raises\n' 'Hamstring Curls\n' 'Romanian Deadlifts\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                   child: Text('Thursday\n' 'Side Leg Raises\n' 'Crunch Kicks\n' 'Raised Leg Circles\n')
-               ),
-             ),
-           ],
-         ),
-         TableRow(
-           decoration: const BoxDecoration(
-           ),
-           children: <Widget>[
-             Container(
-               child: Center(
-                   child: Text('Friday\n' 'Upright Row\n' 'Hammer Curls\n' 'Push Ups\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                   child: Text('Friday\n' 'Leg Extensions\n' 'Steps\n' 'Squat Jumps\n')
-               ),
-             ),
-             Container(
-               child: Center(
-                   child: Text('Friday\n' 'Scissors\n' '20-Count Hold\n' 'Side Elbow Plank\n')
-               ),
+               children: <Widget>[
+                 Container(
+                   child: Center(
+                       child: Text('Friday\n' 'Upright Row\n' 'Hammer Curls\n' 'Push Ups\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Friday\n' 'Leg Extensions\n' 'Steps\n' 'Squat Jumps\n')
+                   ),
+                 ),
+                 Container(
+                   child: Center(
+                       child: Text('Friday\n' 'Scissors\n' '20-Count Hold\n' 'Side Elbow Plank\n')
+                   ),
+                 ),
+               ],
              ),
            ],
          ),
        ],
-     ),
+    ),
      ),
    );
   }
