171752f72d5f572e109eca9e619bbd522c37f7a3
Elijah Yeboah <yeboa037@umn.edu>
2023-11-14 23:42:29 -0600

    Adds messages to firebase but im having an issue where the message string is empty

--- lib/Model/AnnouncementModel.dart
+++ lib/Model/AnnouncementModel.dart
@@ -4,13 +4,27 @@ import 'package:flutter/material.dart';
 class AnnouncementModel {
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController textController = TextEditingController();
 
   String get userName => usernameController.text.trim();
   String get passWord => passwordController.text.trim();
 
+  String get message => textController.text;
+  String time = DateTime.now().toString();
+
   @override
   void getUsername(BuildContext context) async {
-      QuerySnapshot snap = await FirebaseFirestore.instance
-          .collection("Admin").where('Username', isEqualTo: userName).get();
+    QuerySnapshot snap = await FirebaseFirestore.instance
+        .collection("Admin")
+        .where('Username', isEqualTo: userName)
+        .get();
+  }
+
+  @override
+  void addMessage() {
+    FirebaseFirestore.instance.collection("Announcements").add({
+      'Message': message,
+      'Date': time,
+    });
   }
 }
--- lib/Views/Announcements.dart
+++ lib/Views/Announcements.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../Model/AnnouncementModel.dart';
 
@@ -15,30 +16,38 @@ class SelectUser extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Image.asset("images/AnnouncementsIcon.png"),
-              SizedBox(height: 50,),
+              SizedBox(
+                height: 50,
+              ),
               const Text(
                 "Welcome to GetFit Announcements",
                 style: TextStyle(
                   fontSize: 20,
                 ),
               ),
-              SizedBox(height: 25,),
+              SizedBox(
+                height: 25,
+              ),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => AdminPage(
-                        userName: announcementModel.userName,
-                        passWord: announcementModel.passWord,
-                        usernameController: announcementModel.usernameController,
-                        passwordController: announcementModel.passwordController,
-                      )));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => AdminPage(
+                            userName: announcementModel.userName,
+                            passWord: announcementModel.passWord,
+                            usernameController:
+                                announcementModel.usernameController,
+                            passwordController:
+                                announcementModel.passwordController,
+                          )));
                 },
                 child: const Text('Admin'),
                 style: ElevatedButton.styleFrom(
                   elevation: 22,
                 ),
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).push(
@@ -58,7 +67,7 @@ class SelectUser extends StatelessWidget {
 }
 
 class AdminPage extends StatelessWidget {
-  AnnouncementModel a_Model = AnnouncementModel();
+  AnnouncementModel announcementModel = AnnouncementModel();
   final TextEditingController usernameController;
   final TextEditingController passwordController;
   final String userName;
@@ -80,7 +89,9 @@ class AdminPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0,),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 25.0,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -95,7 +106,9 @@ class AdminPage extends StatelessWidget {
                     fontSize: 20,
                   ),
                 ),
-                SizedBox(height: 25,),
+                SizedBox(
+                  height: 25,
+                ),
                 // username textbox
                 TextField(
                   decoration: InputDecoration(
@@ -117,12 +130,16 @@ class AdminPage extends StatelessWidget {
                   controller: usernameController,
                   obscureText: false,
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 // password textbox
                 TextField(
                   decoration: InputDecoration(
                     hintText: 'Enter Password',
-                    hintStyle: const TextStyle(color: Colors.grey,),
+                    hintStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
                     fillColor: Colors.grey[200],
                     filled: true,
                     enabledBorder: OutlineInputBorder(
@@ -140,13 +157,18 @@ class AdminPage extends StatelessWidget {
                   obscureText: true,
                 ),
                 // login button
-                SizedBox(height: 25,),
+                SizedBox(
+                  height: 25,
+                ),
                 GestureDetector(
                   onTap: () {
-                    a_Model.getUsername(context);
-                    Navigator.pushReplacement(context, MaterialPageRoute(
-                        builder: (context) => Announcements())
-                    );
+                    announcementModel.getUsername(context);
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AdminPage2(
+                              textController: announcementModel.textController,
+                            )));
                   },
                   child: Container(
                     padding: const EdgeInsets.all(25),
@@ -175,9 +197,143 @@ class AdminPage extends StatelessWidget {
   }
 }
 
+class AdminPage2 extends StatelessWidget {
+  AnnouncementModel announcementModel = AnnouncementModel();
+  final TextEditingController textController;
+
+  AdminPage2({
+    required this.textController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Page'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('Announcements')
+                      .orderBy(
+                        'Date',
+                        descending: false,
+                      )
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          final post = snapshot.data!.docs[index];
+                          return DisplayAnnouncements(
+                            message: post['Message'],
+                            time: post['Date'],
+                          );
+                        },
+                      );
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Message',
+                          hintStyle: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                          fillColor: Colors.grey[200],
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade200,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade200,
+                            ),
+                          ),
+                        ),
+                        controller: textController,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        announcementModel.addMessage();
+                      },
+                      icon: Icon(Icons.send),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class Announcements extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
+  }
+}
+
+class DisplayAnnouncements extends StatelessWidget {
+  AnnouncementModel announcementModel = AnnouncementModel();
+  final String message;
+  final String time;
+
+  DisplayAnnouncements({
+    required this.message,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.lightBlueAccent[100],
+        borderRadius: BorderRadius.circular(9),
+      ),
+      margin: EdgeInsets.all(25),
+      padding: EdgeInsets.all(25),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(announcementModel.message),
+              Text(announcementModel.time),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
